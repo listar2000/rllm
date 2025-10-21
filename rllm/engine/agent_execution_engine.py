26aa9f918b1d62f1c4700171ee964f7b91268d2e
listar2000 <lisd.star2015@outlook.com>
2025-10-21 03:42:25 -0500

    fix bugs introduced in #256

--- rllm/engine/agent_execution_engine.py
+++ rllm/engine/agent_execution_engine.py
@@ -70,7 +70,11 @@ class AgentExecutionEngine:
         self.max_response_length = max_response_length
         self.max_prompt_length = max_prompt_length
         self.enforce_max_prompt_length = enforce_max_prompt_length
-        self.disable_thinking = self.config.get("rllm", {}).get("disable_thinking", False)
+
+        if self.config is not None:
+            self.disable_thinking = self.config.get("rllm", {}).get("disable_thinking", False)
+        else:  # non-training mode where the config will be empty
+            self.disable_thinking = False
 
         self.agent_class = agent_class
         self.agent_args = agent_args
